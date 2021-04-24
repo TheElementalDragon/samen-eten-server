40bbbe7bd5e46c2261de0ffc995f9f8d04148f4c
TheElementalDragon <60363527+TheElementalDragon@users.noreply.github.com>
2021-04-24 17:42:22 +0200

    Updated gitlab yml and updated js markup

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const http = require('http')
+const http = require("http")
 
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => 
+const server = http.createServer((req, res) =>
 {
-  let result = 
-  {
-    "response" : "Hello World",
-    "status" : "Alles OK!"
-  }
+  let result = {
+    response: "Hello World",
+    status: "Alles OK!",
+  };
   res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader("Content-Type", "application/json")
   res.end(JSON.stringify(result))
 })
 
-server.listen(port, () => 
+server.listen(port, () =>
 {
   console.log(`Server running at http://${port}/`)
 })
